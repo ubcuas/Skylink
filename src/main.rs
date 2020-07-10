daa0d850bac198c871dca5d0b5214eed0cac9f1c
Eric Mikulin <ericm99@gmail.com>
2020-07-09 23:13:21 -0700

    Rust works!

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ use clap::{load_yaml, App};
 use crossbeam::crossbeam_channel;
 use mavlink;
 use ruuas::telem;
-use std::io::{Read, Write, Error, ErrorKind};
+use std::io::Cursor;
+use std::io::{Error, ErrorKind, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-fn telemetry_server(
-    telem_port: u32,
-    telemframe_reciever: crossbeam_channel::Receiver<mavlink::ardupilotmega::MavMessage>,
-) {
+const BUF_SIZE: usize = 256;
+
+struct BufferFrame {
+    data: [u8; BUF_SIZE],
+    length: usize,
+}
+
+fn telemetry_server(telem_port: u32, buffer_reciever: crossbeam_channel::Receiver<BufferFrame>) {
     let mut threads = vec![];
 
     let mut connections = Arc::new(Mutex::new(vec![]));
@@ -32,40 +37,53 @@ fn telemetry_server(
 
     threads.push(thread::spawn({
         let conn_pool = connections.clone();
+        let mut byte_buffer: Vec<u8> = Vec::new();
 
         move || loop {
-            if let Ok(telemframe) = telemframe_reciever.recv() {
-                if let mavlink::ardupilotmega::MavMessage::common(common_msg) = telemframe {
-                    match common_msg {
-                        mavlink::common::MavMessage::GLOBAL_POSITION_INT(gpi_data) => {
-                            let mut conn_pool = conn_pool.lock().unwrap();
+            if let Ok(bufframe) = buffer_reciever.recv() {
+                byte_buffer.extend_from_slice(&bufframe.data);
+                loop {
+                    let read_byte_buffer = byte_buffer.clone();
+                    let mut buff = Cursor::new(read_byte_buffer);
 
-                            for mut conn in conn_pool.iter() {
-                                let telem_args = telem::TelemetryArgs {
-                                    latitude: gpi_data.lat,
-                                    longitude: gpi_data.lon,
-                                    altitude_agl_meters: gpi_data.relative_alt as i32,
-                                    altitude_msl_meters: gpi_data.alt as i32,
-                                    heading_degrees: gpi_data.hdg as u32,
-                                    velocity_x_cm_s: gpi_data.vx as i32,
-                                    velocity_y_cm_s: gpi_data.vy as i32,
-                                    velocity_z_cm_s: gpi_data.vz as i32,
-                                    timestamp_telem_ms: gpi_data.time_boot_ms as u64,
-                                    /* EMPTY RN */
-                                    roll_rad: 0.0,
-                                    pitch_rad: 0.0,
-                                    yaw_rad: 0.0,
-                                    rollspeed_rad_s: 0.0,
-                                    pitchspeed_rad_s: 0.0,
-                                    yawspeed_rad_s: 0.0,
-                                    timestamp_msg_ms: 0,
-                                };
-                                let msg = telem::new_telem_msg(telem_args);
-                                let data = telem::serialize_telem_msg(msg);
-                                conn.write_all(&data);
+                    if let Ok((_parsed_header, parsed_msg)) = mavlink::read_v2_msg(&mut buff) {
+                        match parsed_msg {
+                            mavlink::common::MavMessage::GLOBAL_POSITION_INT(gpi_data) => {
+                                let mut conn_pool = conn_pool.lock().unwrap();
+
+                                for mut conn in conn_pool.iter() {
+                                    let telem_args = telem::TelemetryArgs {
+                                        latitude: gpi_data.lat,
+                                        longitude: gpi_data.lon,
+                                        altitude_agl_meters: gpi_data.relative_alt as i32,
+                                        altitude_msl_meters: gpi_data.alt as i32,
+                                        heading_degrees: gpi_data.hdg as u32,
+                                        velocity_x_cm_s: gpi_data.vx as i32,
+                                        velocity_y_cm_s: gpi_data.vy as i32,
+                                        velocity_z_cm_s: gpi_data.vz as i32,
+                                        timestamp_telem_ms: gpi_data.time_boot_ms as u64,
+                                        /* EMPTY RN */
+                                        roll_rad: 0.0,
+                                        pitch_rad: 0.0,
+                                        yaw_rad: 0.0,
+                                        rollspeed_rad_s: 0.0,
+                                        pitchspeed_rad_s: 0.0,
+                                        yawspeed_rad_s: 0.0,
+                                        timestamp_msg_ms: 0,
+                                    };
+                                    let msg = telem::new_telem_msg(telem_args);
+                                    let data = telem::serialize_telem_msg(msg);
+                                    conn.write_all(&data);
+                                }
                             }
+                            _ => {}
                         }
-                        _ => {}
+
+                        byte_buffer.clear();
+                        let start = buff.position() as usize;
+                        byte_buffer.extend_from_slice(&buff.into_inner()[start..]);
+                    } else {
+                        break;
                     }
                 }
             }
@@ -108,34 +126,43 @@ fn main() -> std::io::Result<()> {
     println!("Loop fully connected");
 
     let mut threads = vec![];
-    // let (telemframe_sender, telemframe_reciever) = crossbeam_channel::unbounded();
+    let (buffer_sender, buffer_reciever) = crossbeam_channel::unbounded();
 
-    let vehicle = Arc::new(Mutex::new(mav_src));
-    let gcs = Arc::new(Mutex::new(mav_dest));
+    mav_src.set_nonblocking(true)?;
+    mav_dest.set_nonblocking(true)?;
+    let mut vehicle = mav_src;
+    let mut gcs = mav_dest;
 
     threads.push(thread::spawn({
-        let vehicle = vehicle.clone();
-        let gcs = gcs.clone();
         move || loop {
-            let mut buf: [u8; 1024] = [0; 1024];
-            let bytes_read: usize = vehicle.lock().unwrap().read(&mut buf).unwrap();
-            gcs.lock().unwrap().write_all(&buf[..bytes_read]).unwrap();
+            let mut buf: [u8; BUF_SIZE] = [0; BUF_SIZE];
+            match vehicle.read(&mut buf) {
+                Ok(bytes_read) => {
+                    gcs.write_all(&buf[..bytes_read]).unwrap();
+                    let payload = BufferFrame {
+                        data: buf,
+                        length: bytes_read,
+                    };
+                    buffer_sender.send(payload);
+                }
+                Err(ref e) if e.kind() == ErrorKind::WouldBlock => {}
+                Err(e) => panic!("encountered IO error: {}", e),
+            };
+
+            let mut buf: [u8; BUF_SIZE] = [0; BUF_SIZE];
+            match gcs.read(&mut buf) {
+                Ok(bytes_read) => {
+                    vehicle.write_all(&buf[..bytes_read]).unwrap();
+                }
+                Err(ref e) if e.kind() == ErrorKind::WouldBlock => {}
+                Err(e) => panic!("encountered IO error: {}", e),
+            };
         }
     }));
 
-    threads.push(thread::spawn({
-        let vehicle = vehicle.clone();
-        let gcs = gcs.clone();
-        move || loop {
-            let mut buf: [u8; 1024] = [0; 1024];
-            let bytes_read: usize = gcs.lock().unwrap().read(&mut buf).unwrap();
-            vehicle.lock().unwrap().write_all(&buf[..bytes_read]).unwrap();
-        }
+    threads.push(thread::spawn(move || {
+        telemetry_server(telemdest_port, buffer_reciever)
     }));
-
-    // threads.push(thread::spawn(move || {
-    //     telemetry_server(telemdest_port, telemframe_reciever)
-    // }));
 
     for thread in threads {
         thread.join().unwrap();
